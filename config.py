1d97c342d662f8baa1b6828a1743e44d8bf964c5
wheelebin <joakim.allen@hotmail.com>
2014-05-07 17:55:07 +0200

    Added some files

---
+++ config.py
@@ -0,0 +1,13 @@
+class configuration(object):
+	def __init__(self):
+		s = self
+		s.server = 'irc.twitch.tv'
+		s.port 6667
+		s.readbuffer = ""
+
+		#Edit according to your details
+		s.channel = "#richhomiej"
+		s.user = "mcnextbot"
+		s.passw = "oauth:70m2l7mm5qmxh5hv5he9764qli6sjdg"
+
+		
